6ef90a3da170818d3d40e583900b98df23e85be5
Ashley Heilman <ajheilman@lcmail.lcsc.edu>
2016-12-03 21:40:15 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+using std::cout;
+
+int main()
+{
+    for (int bottles(10); bottles > 0; bottles -= 1){
+        if (bottles == 1){
+            cout <<bottles<< " bottle of beer on the wall\n";
+            cout <<bottles<< " bottle of beer\n";
+            cout <<"Take one down, pass it around\n";
+            cout <<"There are no more bottles of beer on the wall :-(\n\n";
+        }
+        else{
+            cout <<bottles<< " bottles of beer on the wall\n";
+            cout <<bottles<< " bottles of beer\n";
+            cout <<"Take one down, pass it around\n";
+            cout <<bottles<< " bottles of beer on the wall\n\n";
+        }
+    }
+}
