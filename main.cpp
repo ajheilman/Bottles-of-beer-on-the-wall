850abea12c52b810a185359087de9c5d9dbb33ee
Ashley Heilman <ajheilman@lcmail.lcsc.edu>
2017-01-15 20:55:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 
 int main()
 {
-    for (int bottles(10); bottles > 0; bottles -= 1){
+    for (int bottles(99); bottles > 0; bottles -= 1){
         if (bottles == 1){
             cout <<bottles<< " bottle of beer on the wall\n";
             cout <<bottles<< " bottle of beer\n";
